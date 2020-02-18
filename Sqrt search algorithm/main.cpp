23e4edecb64b8fe2fffe424d23cb210c1ca1c6aa
Dima <61165268+Kolodiychyuk@users.noreply.github.com>
2020-02-19 00:08:13 +0200

    sqrt search algorithm

--- Sqrt search algorithm/main.cpp
+++ Sqrt search algorithm/main.cpp
@@ -4,10 +4,10 @@ double SSA(double);
 using namespace std;
 int main() {
 	cout << "Sqrt(25): "  << SSA(25) << "\n"
-             << "Sqrt(11): "  << FAS(11) << "\n"
+             << "Sqrt(11): "  << SSA(11) << "\n"
 	     << "Sqrt(31): "  << SSA(31) << "\n"
 	     << "Sqrt(46): "  << SSA(46) << "\n"
-             << "Sqrt(64): "  << FAS(64) << "\n"
+             << "Sqrt(64): "  << SSA(64) << "\n"
 	     << "Sqrt(72): "  << SSA(72) << "\n";
 	return 0;
 }
